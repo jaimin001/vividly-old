398ac23e8945b15afec71593d2f73e344c8ecaff
Jaimin Gajjar <jaimingajjar01@gmail.com>
2021-11-15 02:57:14 +0530

    Used AbstractUser to create User Model

--- vividly/texting/models.py
+++ vividly/texting/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 from django.utils import tree
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=10, blank=False, unique=True, primary_key=True)
-    first_name = models.CharField(max_length=20, blank=False)
-    last_name = models.CharField(max_length=20, blank=False)
-    email = models.EmailField(blank=True)
-    password = models.CharField(max_length=20, blank=False)
+class User(AbstractUser):
+    profile_picture = models.ImageField(name='pfp')
 
     def __str__(self):
         return self.username
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='receiver')
     textMessage = models.TextField(max_length=1200 ,null=True, blank=True, default=None)
     file = models.FileField(upload_to=None, default=None)
     timestamp = models.DateTimeField(auto_now_add=True)
