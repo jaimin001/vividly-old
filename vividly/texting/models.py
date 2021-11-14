7595d4074be545296c0751e15f2d515049e7831f
Jaimin Gajjar <jaimingajjar01@gmail.com>
2021-11-15 00:59:18 +0530

    Added a User model and FileField in Message model

--- vividly/texting/models.py
+++ vividly/texting/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.utils import tree
-from django.contrib.auth import get_user_model
-
 
 # Create your models here.
-User = get_user_model()
+class User(models.Model):
+    username = models.CharField(max_length=10, blank=False, unique=True, primary_key=True)
+    first_name = models.CharField(max_length=20, blank=False)
+    last_name = models.CharField(max_length=20, blank=False)
+    email = models.EmailField(blank=True)
+    password = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.username
 
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     textMessage = models.TextField(max_length=1200 ,null=True, blank=True, default=None)
+    file = models.FileField(upload_to=None, default=None)
     timestamp = models.DateTimeField(auto_now_add=True)
     is_seen = models.BooleanField(default=False)
 
